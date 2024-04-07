ef7d68cae838d2dea4de11e752ec23a3384c7e20
the90srp <158795531+the90srp@users.noreply.github.com>
2024-04-07 16:48:31 -0400

    nginx

---
+++ nginx/html/js/drop_down.js
@@ -0,0 +1,64 @@
+// JavaScript Document
+
+<!--
+function mmLoadMenus() {
+  if (window.mm_menu_0910214742_0) return;
+                                              window.mm_menu_0910214742_0 = new Menu("root",180,20,"Verdana, Arial, Helvetica, sans-serif",12,"#FFFFFF","#FFFFFF","#0079C2","#5fc3ff","left","middle",3,0,1000,-5,7,true,true,true,0,false,false);
+  mm_menu_0910214742_0.addMenuItem("Overview","location='NYPD_BenefitsOverview.aspx'");
+  mm_menu_0910214742_0.addMenuItem("Military&nbsp;Benefits","location='NYPD_military_benefits.aspx'");
+  mm_menu_0910214742_0.addMenuItem("Promotional&nbsp;Opportunities","location='NYPD_PromotionalOpportunites.aspx'");
+  mm_menu_0910214742_0.addMenuItem("Retirement&nbsp;Benefits","location='NYPD_RetirementBenefits.aspx'");
+  mm_menu_0910214742_0.addMenuItem("Educational&nbsp;Opportunities","location='NYPD_EducationalOpportunities.aspx'");
+  mm_menu_0910214742_0.addMenuItem("College&nbsp;Resources","location='NYPD_CollegeResources.aspx'");
+   mm_menu_0910214742_0.hideOnMouseOut=true;
+   mm_menu_0910214742_0.bgColor='#000000';
+   mm_menu_0910214742_0.menuBorder=1;
+   mm_menu_0910214742_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_0910214742_0.menuBorderBgColor='#000000';
+window.mm_menu_0910222736_0 = new Menu("root",145,20,"Verdana, Arial, Helvetica, sans-serif",12,"#FFFFFF","#FFFFFF","#0079C2","#5fc3ff","left","middle",3,0,1000,-5,7,true,true,true,0,true,true);
+  mm_menu_0910222736_0.addMenuItem("Overview","location='NYPD_AbouttheAcademy.aspx'");
+  mm_menu_0910222736_0.addMenuItem("The&nbsp;Hiring&nbsp;Process","location='NYPD_TheHiringProcess.aspx'");
+  mm_menu_0910222736_0.addMenuItem("Job&nbsp;Standard&nbsp;Test","location='NYPD_JobStandardTest.aspx'");
+  mm_menu_0910222736_0.addMenuItem("JST&nbsp;Training&nbsp;Tips","location='NYPD_JSTTrainingTips.aspx'");
+  mm_menu_0910222736_0.addMenuItem("Relocation&nbsp;Assistance","location='NYPD_RelocationAssistance.aspx'");
+   mm_menu_0910222736_0.addMenuItem("Bulletin&nbsp;Board","location='Bulletin_Board.aspx'");
+   mm_menu_0910222736_0.hideOnMouseOut=true;
+   mm_menu_0910222736_0.bgColor='#000000';
+   mm_menu_0910222736_0.menuBorder=1;
+   mm_menu_0910222736_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_0910222736_0.menuBorderBgColor='#000000';
+window.mm_menu_0910223034_0 = new Menu("root",145,20,"Verdana, Arial, Helvetica, sans-serif",12,"#FFFFFF","#FFFFFF","#0079C2","#5fc3ff","left","middle",3,0,1000,-5,7,true,true,true,0,true,true);
+  mm_menu_0910223034_0.addMenuItem("Overview","location='NYPD_InsidetheNYPD.aspx'");
+  mm_menu_0910223034_0.addMenuItem("300&nbsp;Units","location='NYPD_units.aspx'");
+  mm_menu_0910223034_0.addMenuItem("NYPD&nbsp;Sports","location='NYPD_sports.aspx'");
+  mm_menu_0910223034_0.addMenuItem("Civilian&nbsp;Opportunities","location='NYPD_civilians.aspx'");
+  mm_menu_0910223034_0.addMenuItem("In&nbsp;The&nbsp;News","location='NYPD_in_the_news.aspx'");
+  mm_menu_0910223034_0.addMenuItem("NYPD&nbsp;History","location='NYPD_History.aspx'");
+    
+   mm_menu_0910223034_0.hideOnMouseOut=true;
+   mm_menu_0910223034_0.bgColor='#000000';
+   mm_menu_0910223034_0.menuBorder=1;
+   mm_menu_0910223034_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_0910223034_0.menuBorderBgColor='#000000';
+  window.mm_menu_0910223324_0 = new Menu("root",150,20,"Verdana, Arial, Helvetica, sans-serif",12,"#FFFFFF","#FFFFFF","#0079C2","#5fc3ff","left","middle",3,0,1000,-5,7,true,true,true,0,true,true);
+  //mm_menu_0910223324_0.addMenuItem("Apply&nbsp;Now","location='https://www425.ssldomain.com/nypd2/recruit/recruittest.cfm'");
+  mm_menu_0910223324_0.addMenuItem("FAQ","location='NYPD_FrequentlyAskedQuestions.aspx'");
+  mm_menu_0910223324_0.addMenuItem("Exam&nbsp;and&nbsp;Employment","location='NYPD_ExamandEmploymentRequirements.aspx'");
+   mm_menu_0910223324_0.hideOnMouseOut=true;
+   mm_menu_0910223324_0.bgColor='#000000';
+   mm_menu_0910223324_0.menuBorder=1;
+   mm_menu_0910223324_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_0910223324_0.menuBorderBgColor='#000000';
+  window.mm_menu_0910223351_0 = new Menu("root",145,20,"Verdana, Arial, Helvetica, sans-serif",12,"#FFFFFF","#FFFFFF","#0079C2","#5fc3ff","left","middle",3,0,1000,-5,7,true,true,true,0,true,true);
+  mm_menu_0910223351_0.addMenuItem("Exam&nbsp;Overview","location='NYPD_ExamOverview.aspx'");
+  mm_menu_0910223351_0.addMenuItem("Exam&nbsp;Tutorials","location='NYPD_ExamTutorials.aspx'");
+  mm_menu_0910223351_0.addMenuItem("Exam&nbsp;Answer&nbsp;Keys","location='NYPD_ExamAnswerKeys.aspx'");
+   mm_menu_0910223351_0.hideOnMouseOut=true;
+   mm_menu_0910223351_0.bgColor='#000000';
+   mm_menu_0910223351_0.menuBorder=1;
+   mm_menu_0910223351_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_0910223351_0.menuBorderBgColor='#000000';
+
+mm_menu_0910223351_0.writeMenus();
+} // mmLoadMenus()
+//-->
